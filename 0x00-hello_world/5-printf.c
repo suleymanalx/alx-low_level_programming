82ff6b524354f4a18cdf259c1835d51a0580be68
suleymanalx <suleymanalx@gmail.com>
2022-06-02 22:06:27 +0300

    update file 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,16 @@
-#include <stdio.h>
-/*
- * Main Function
+/**
+ * main - starting point of the program
+ * void - doesn't accept a value
+ * Description: prints text on the consule
+ * Section header:
+ * Return: it will return 0 at successful completion
  */
-
+#include <stdio.h>
 int main (void)
 {
-  /* Print Message */
+/* Print Message */
 printf("with proper grammar, but the outcome is a piece of art,");
-
+/* return 0 */
 return (0);
 
 }
