cfcfc1023baa292a1ed23cefd3c0c17722284777
suleymanalx <suleymanalx@gmail.com>
2022-06-02 09:38:08 +0300

    print using prinf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * Main Function
+ */
+
+int main (void)
+{
+  /* Print Message */
+  printf("with proper grammar, but the outcome is a piece of art,");
+
+  return (0);
+
+}
