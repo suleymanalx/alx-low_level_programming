41732cc463d3e63c263e74f625a201ee6dc8966e
suleymanalx <suleymanalx@gmail.com>
2022-06-02 22:26:41 +0300

    update file 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,11 @@
 int main(void)
 {
 /* Print size of a variable */
-printf("size of a char: %u bytes(s)\n", sizeof(char));
-printf("size of an int: %u bytes(s)\n", sizeof(int));
-printf("size of a long int: %u bytes(s)\n", sizeof(long int));
-printf("size of a long long int: %u bytes(s)\n", sizeof(long long int));
-printf("size of a float: %u bytes(s)\n", sizeof(float));
+printf("size of a char: %d bytes(s)\n", sizeof(char));
+printf("size of an int: %d bytes(s)\n", sizeof(int));
+printf("size of a long int: %d bytes(s)\n", sizeof(long int));
+printf("size of a long long int: %d bytes(s)\n", sizeof(long long int));
+printf("size of a float: %d bytes(s)\n", sizeof(float));
 
 return (0);
 }
