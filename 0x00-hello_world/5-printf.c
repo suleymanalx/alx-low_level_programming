95225e51744c0aa6923c4eb301c616c2c785b621
suleymanalx <suleymanalx@gmail.com>
2022-06-02 21:04:56 +0300

    update file 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,8 +6,8 @@
 int main (void)
 {
   /* Print Message */
-  printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,");
 
-  return (0);
+return (0);
 
 }
