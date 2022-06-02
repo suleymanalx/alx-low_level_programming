a30ac937d2d20f882ecfd9d95e50cf03b8a45367
suleymanalx <suleymanalx@gmail.com>
2022-06-02 22:19:38 +0300

    size of different data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
-
-/*
- * Main Function
+/**
+ * main - program starting point
+ * void - function main does not accept a value
+ * Description: a program displays size of variables
+ * Section header:
+ * Return: function main returns 0 for successful completion
  */
-
-int main (void)
+int main(void)
 {
-    /* Print size of a variable */
-    printf("size of a char: %u bytes(s)", sizeof(char));
-    printf("size of an int: %u bytes(s)", sizeof(int));
-    printf("size of a long int: %u bytes(s)", sizeof(long int));
-    printf("size of a long long int: %u bytes(s)", sizeof(long long int));
-    printf("size of a float: %u bytes(s)", sizeof(float));
+/* Print size of a variable */
+printf("size of a char: %u bytes(s)", sizeof(char));
+printf("size of an int: %u bytes(s)", sizeof(int));
+printf("size of a long int: %u bytes(s)", sizeof(long int));
+printf("size of a long long int: %u bytes(s)", sizeof(long long int));
+printf("size of a float: %u bytes(s)", sizeof(float));
 
-    return (0);
+return (0);
 }
