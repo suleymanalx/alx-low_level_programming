5f464bb41261c4433ec79ce16502470acc1be5ca
suleymanalx <suleymanalx@gmail.com>
2022-06-02 18:39:14 +0300

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
 /*
- * Main fuction
+ * String printing program
  */
+#include <stdio.h>
 
+/* main function */
 int main(void)
 {
   /* Print Messege */
-  puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 
-  return (0);
+return (0);
 
 }
