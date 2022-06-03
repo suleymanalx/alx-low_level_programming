1be3239eeba69ed6f98c221de6af28ce08e1054c
suleymanalx <suleymanalx@gmail.com>
2022-06-03 21:32:59 +0300

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Main Function
+ * void - main function does not accept any parameter
+ *
+ * Description: prints out digits on consule
+ * Return: main returns 0 at successful compeletion
+ */
+
+int main(void)
+{
+int i;
+for (i = 49; i < 59; i++)
+{
+putchar(i);
+}
+putchar('\n');
+
+return (0);
+}
