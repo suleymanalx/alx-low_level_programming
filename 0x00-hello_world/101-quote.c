bd8a4e00d7115b9a13d1cbbefddf8f2e35516a3f
suleymanalx <suleymanalx@gmail.com>
2022-06-03 08:48:32 +0300

    print quote updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,14 @@
 /**
  * main - starting point ofprogram
  * void - main does not accept any value
+ * Section header:
  * Description: program displays text on consule
- * return: the program returns 1
+ * Return: the program returns 1 for successful completion
  */
-int main (void)
+int main(void)
 {
-  puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-  return (1);
+/* prints on consul */
+puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+/* returns 1 when compelte */
+return (1);
 }
