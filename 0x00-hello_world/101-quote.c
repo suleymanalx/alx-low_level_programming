2de8e3ee6b1910b1f1500e36a8ef213db3540b3e
suleymanalx <suleymanalx@gmail.com>
2022-06-03 08:41:13 +0300

    print quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - starting point ofprogram
+ * void - main does not accept any value
+ * Description: program displays text on consule
+ * return: the program returns 1
+ */
+int main (void)
+{
+  puts("and that piece of art is useful\ - Dora Korpar, 2015-10-19");
+  return (1);
+}
