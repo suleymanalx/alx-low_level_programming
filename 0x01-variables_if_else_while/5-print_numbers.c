c1984ec96b183b336a6c201bce50d93692248f24
suleymanalx <suleymanalx@gmail.com>
2022-06-03 16:10:14 +0300

    print digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Main Function
+ * void - main function does not accept any parameter
+ *
+ * Description: prints out characters in small latter
+ * Return: main returns 0 at successful compeletion
+ */
+
+int main(void)
+{
+int digit;
+for (digit = 0; digit < 10; digit++)
+{
+put (digit);
+}
+putchar ('\n');
+
+return (0);
+}
