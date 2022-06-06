c5cd64b5d69f47dc0b37bdf1aa3b3e459ec97345
suleymanalx <suleymanalx@gmail.com>
2022-06-06 22:07:19 +0300

    print combination 4 101.1

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,23 +10,29 @@
 
 int main(void)
 {
-int p, q;
-for (p = 0; p <= 98; p++)
+int p, q, r;
+for (p = 0; p <= 97; p++)
 {
-for (q = p + 1; q <= 99; q++)
+
+for (q = p + 1; q <= 98; q++)
 {
+
+for (r = q + 1; r <= 99; r++)
+{
+
 putchar((p / 10) + '0');
-putchar((p % 10) + '0');
-putchar(' ');
 putchar((q / 10) + '0');
-putchar((q % 10) + '0');
+putchar((r / 10) + '0');
 
 if (p == 98 && q == 99)
 continue;
 putchar(',');
 putchar(' ');
+
 }
 }
+}
+
 putchar ('\n');
 return (0);
 }
