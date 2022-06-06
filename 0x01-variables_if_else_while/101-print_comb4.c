ba5503b09f9c17d04b8929fa33e7d0b088301aee
suleymanalx <suleymanalx@gmail.com>
2022-06-06 22:11:00 +0300

    print combination 4 101.2

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,20 +11,20 @@
 int main(void)
 {
 int p, q, r;
-for (p = 0; p <= 97; p++)
+for (p = 0; p <= 7; p++)
 {
 
-for (q = p + 1; q <= 98; q++)
+for (q = p + 1; q <= 8; q++)
 {
 
-for (r = q + 1; r <= 99; r++)
+for (r = q + 1; r <= 9; r++)
 {
 
-putchar((p / 10) + '0');
-putchar((q / 10) + '0');
-putchar((r / 10) + '0');
+putchar((p) + '0');
+putchar((q) + '0');
+putchar((r) + '0');
 
-if (p == 98 && q == 99)
+if (p == 8 && q == 9)
 continue;
 putchar(',');
 putchar(' ');
