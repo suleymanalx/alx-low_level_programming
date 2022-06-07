33f22d74dd4d7e8da3b829c057d8cdf627f2b6bc
suleymanalx <suleymanalx@gmail.com>
2022-06-07 22:10:39 +0300

    printing using _putchar()

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+/**
+ * main - Main Function
+ * void - main function does not accept any parameter
+ *
+ * Description: prints out digits on consule using _putchar()
+ * Return: main returns 0 at successful compeletion
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
