1fed01ccd659d0e6ec7c204be1151b91e41736aa
suleymanalx <suleymanalx@gmail.com>
2022-06-09 21:00:25 +0300

    print the sign of the digit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * sign -  Function
+ * @n - sign function accepts a int parameter
+ *
+ * Description: prints out +, 0 or - based on the digit value
+ * Return: returns 1, 0 or -1 base on the  condition
+ */
+
+int _sign (int n)
+{
+if (n > 0)
+{
+_putchar ('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar ('0');
+return (0);
+}
+else
+{
+_putchar ('-');
+return (-1);
+}
+}
