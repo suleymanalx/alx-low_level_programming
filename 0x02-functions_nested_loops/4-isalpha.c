47fa863bdc259a92f9f6f3e49612806dde34d010
suleymanalx <suleymanalx@gmail.com>
2022-06-09 19:47:47 +0300

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha -  Function
+ * char - _isalpha function accepts char as parameter
+ * @c : accepts character c as parameter
+ * Description: returns 0 for non char 1 for other
+ * Return: _isalpha returns 0 or 1 based on the condition
+ */
+int _isalpha(char c)
+{
+if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
