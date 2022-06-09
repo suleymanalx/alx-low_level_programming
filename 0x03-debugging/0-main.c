e4b13e91d636ac19312a61e45772a5adb1286bc7
suleymanalx <suleymanalx@gmail.com>
2022-06-09 21:56:51 +0300

    debugging 0.2

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,11 +2,12 @@
 /**
  * main - function
  * @void
- * 
+ * Description: test the positive_or_negative function
  * @return int 
  */
 int main(void)
 {
+/* checks sign of the digit */
 positive_or_negative(0);
 return (0);
 }
